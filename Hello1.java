602b9423e27d4007450d277c0caae982db7ecb95
Saughat123 <147581806+Saughat123@users.noreply.github.com>
2023-11-17 23:02:30 +0530

    Add files via upload

---
+++ Hello1.java
@@ -0,0 +1,7 @@
+class Hello1{  
+    public static void main(String[] args){  
+    System.out.println("This is java app");  
+System.out.println("This is java app by using jenkins"); 
+System.out.println("This is java app 2nd build");  
+    }  
+    }  
